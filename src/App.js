804fcd8c472ad680f1bd9960813f67290ec8a573
Your <you@example.com>
2023-02-10 10:58:20 +0600

    Email password Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.init';
 import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 
 function App() {
@@ -47,25 +48,54 @@ function App() {
 
   const handleRegister = (event) => {
     event.preventDefault();
-    console.log(event.target.email.value);
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        // Signed in 
+        const user = result.user;
+        setUser(result.user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log('error', error.message);
+        setUser({});
+      });
   }
 
   return (
-    <div className='App'>
+    <div  className='w-50 mx-auto !important'>
 
-      <form onSubmit={handleRegister}>
+      {/* <form onSubmit={handleRegister}>
         <input type="email" name='email' id='email' placeholder='Your Email' />
         <br />
         <input type="password" name='password' id='password' placeholder='Your Password' />
         <br />
         <button type='submit'>Register</button>
-      </form>
+      </form> */}
 
       {
         user.uid ?
           <button onClick={handleSignOut}>Sign Out</button>
           :
           <>
+
+            <Form onSubmit={handleRegister}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Password" />
+              </Form.Group>
+              
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+
             <button onClick={handleGoogleSignIn}>Google sign in</button>
             <button onClick={handleGithubSignIn}>Github sign in</button>
           </>
